65c4035a18c1b703e8aea050c90a70d4491f3159
Getinet Amare <getinetamare@gmail.com>
2023-01-11 18:44:57 +0300

    Update a file

--- Back End/Card/src/test/java/com/cbe/card/customer/CustomerControllerTest.java
+++ Back End/Card/src/test/java/com/cbe/card/customer/CustomerControllerTest.java
@@ -4,13 +4,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CustomerControllerTest {
     Calculator calculator = new Calculator();
+
     @Test
     @DisplayName("Test Two Numbers")
    void testAddTwoNumbers(){
        assertThat(calculator.addTwoNumbers(210,30)).isEqualTo(240);
+   }
+   @Test
+   @DisplayName("Test Two Numbers using assert equal")
+   void testAddTwoNumbersA(){
+       assertEquals(calculator.addTwoNumbers(12,12), 24);
    }
 }
 class Calculator{
